abc57d405838de05803ef84b5c4b49fd5201ecb5
Cesar Alva <csarx09@gmail.com>
2022-07-09 00:32:10 -0500

    Portfolio

---
+++ app/src/main/java/com/example/cvmobileapp/BackgroundActivity.kt
@@ -0,0 +1,26 @@
+package com.example.cvmobileapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cvmobileapp.databinding.ActivityBackgroundBinding
+import com.example.cvmobileapp.databinding.ActivityMainBinding
+
+class BackgroundActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityBackgroundBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBackgroundBinding.inflate((layoutInflater))
+        setContentView(R.layout.activity_background)
+        setContentView(binding.root);
+        binding.backReturn.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
